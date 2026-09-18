12a044acae9b891893a9f0bec7562497a456ad88
realincor <saurabhrathee95@gmail.com>
2026-09-18 11:03:55 +0000

    addded/ui dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +13,9 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
